1f3ea8605c25838f1763a424b76c6d70116d3252
tomkimra <tomkimra@gmail.com>
2018-11-05 17:53:42 +0900

    change server-worker

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,30 @@
+// cache
+var cacheName = 'pwatest-1';
+var filesToCache = [];
+
 // service-worker.js
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        console.log('[ServiceWorker] Removing old cache', key);
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
 });
 
 // 現状では、この処理を書かないとService Workerが有効と判定されないようです
